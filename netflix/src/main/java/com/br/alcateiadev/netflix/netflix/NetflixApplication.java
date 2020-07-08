b9891c3334c553a5b981254275f8f0b0db920209
Marcelo Vieira <marcelo.vieira@evoluum.com.br>
2020-07-08 09:09:18 -0300

    trocando config do netflix

--- netflix/src/main/java/com/br/alcateiadev/netflix/netflix/NetflixApplication.java
+++ netflix/src/main/java/com/br/alcateiadev/netflix/netflix/NetflixApplication.java
@@ -2,13 +2,14 @@ package com.br.alcateiadev.netflix.netflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-//@EnableZuulProxy
+@EnableZuulProxy
 public class NetflixApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(NetflixApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixApplication.class, args);
+	}
 
 }
